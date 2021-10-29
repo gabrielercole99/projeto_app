92e06b02828a8ea4f08421e0330f091aa5de3ae7
gabrielercole <g.f.a.ercole@gmail.com>
2021-10-28 21:14:47 -0300

    completando aplicação

--- app_trilha/src/app/base/home/home.page.ts
+++ app_trilha/src/app/base/home/home.page.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
+
+  travel: any[] = [ {name: " Parque Nacional da Serra dos Órgãos - Teresópolis", done: false, 
+                      src: "../../assets/img/nacional.jpg",
+                      conted:"trilha moderada, Duração: 3 horas"},
+                    {name: " Uaná Etê Jardim Ecológico - Vassouras", done: false, 
+                      src: "../../../assets/img/uana2.jpg"},
+                    {name: " Vale do Alcantilado - Visconde de Mauá", done: false, 
+                      src: "../../../assets/img/vale.jpg"},
+                    {name: " Cachoeira Rio do Ouro - Magé", done: false, 
+                      src: "../../../assets/img/cacho.jpg"},
+                    {name: " Costão de Itacoatiara - Niterói", done: false, 
+                      src: "../../../assets/img/costao.jpg"},
+                    {name: " Pico da Caledônia - Nova Friburgo", done: false, 
+                      src:"../../../assets/img/pico.jpg"},
+                    {name: " Pedra do Macaco - Maricá", done: false, 
+                      src: "../../../assets/img/pedra.jpg"},
+                    {name: " Alto do Gaia - São Gonçalo", done: false, 
+                      src: "../../../assets/img/gaia.jpg"},
+                    {name: " Parque Estadual do Cunhambebe - Mangaratiba", done: false, 
+                      src: "../../../assets/img/cunha.jpg"},
+                    {name: " Travessia Serra do Piloto x Mazomba - Mangaratiba", done: false, 
+                      src: "../../../assets/img/serra.jpg"},];
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
--- app_trilha/src/app/base/list/list.page.ts
+++ app_trilha/src/app/base/list/list.page.ts
@@ -6,9 +6,26 @@ import { ActionSheetController } from '@ionic/angular';
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
 })
-export class ListPage implements OnInit {
 
-  constructor(private actionControl: ActionSheetController) { }
+export class ListPage {
+
+  travel: any[] = [ {name: ' Parque Nacional da Serra dos Órgãos - Teresópolis', done: false},
+                    {name: ' Uaná Etê Jardim Ecológico - Vassouras', done: false},
+                    {name: ' Vale do Alcantilado - Visconde de Mauá', done: false},
+                    {name: ' Cachoeira Rio do Ouro - Magé', done: false},
+                    {name: ' Costão de Itacoatiara - Niterói', done: false},
+                    {name: ' Pico da Caledônia - Nova Friburgo', done: false},
+                    {name: ' Pedra do Macaco - Maricá', done: false},
+                    {name: ' Alto do Gaia - São Gonçalo', done: false},
+                    {name: ' Parque Estadual do Cunhambebe - Mangaratiba', done: false},
+                    {name: ' Travessia Serra do Piloto x Mazomba - Mangaratiba', done: false},];
+
+  constructor(private actionControl: ActionSheetController) { 
+
+
+    this.readLocalStorage();
+    
+  }
 
   async openActions(item: any) {
     const actionSheet = await this.actionControl.create({
@@ -31,4 +48,18 @@ export class ListPage implements OnInit {
       ]
     });
 
+    actionSheet.present();
+  }
+
+  updateLocalStorage(){
+    localStorage.setItem('travelDB',JSON.stringify(this.travel));
+  }
+  
+  readLocalStorage(){
+    let travelJson = localStorage.getItem('travelDB');
+
+    if(travelJson != null){
+      this.travel = JSON.parse(travelJson);
+    }
+  }
 }
